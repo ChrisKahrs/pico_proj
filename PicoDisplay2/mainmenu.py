596cad5ec1ec48f9c346588c6aab029296a434af
Chris Kahrs <chriskahrs@msn.com>
2023-08-15 21:27:08 -0400

    number menus

--- PicoDisplay2/mainmenu.py
+++ PicoDisplay2/mainmenu.py
@@ -1,5 +1,4 @@
-from pimoroni import Button
-from pimoroni import RGBLED
+from pimoroni import Button, RGBLED
 from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY_2, PEN_P4
 import gc
 import time
@@ -19,13 +18,10 @@ GREY = (128, 128, 128)
 CYAN = (0, 255, 255)
 BROWN = (165, 42, 42)
 
-class g:
+class g: # globals for display parts
     display = None
     led = None
-    screen_width = 0
-    screen_height = 0
-    top_text_buffer = 5
-    side_text_buffer = 5
+    brightness = 0.5
         
 # pin layouts
 button_a = Button(12)
@@ -49,32 +45,34 @@ def free(full=False):
 
 def set_brightness(color):
     # Adjust the brightness of the RGB values in the color tuple
-    r, gg, b = color
-    r = int(r * g.brightness)  # Scale the red value
-    gg = int(gg * g.brightness)  # Scale the green value
-    b = int(b * g.brightness)  # Scale the blue value
-    return (r, gg, b)  # Return the adjusted color tuple
+    red, green, blue = color
+    red = int(red * g.brightness)  # Scale the red value
+    green = int(green * g.brightness)  # Scale the green value
+    blue = int(blue * g.brightness)  # Scale the blue value
+    return (red, green, blue)  # Return the adjusted color tuple
 
 def run_menu():
     # read json file into menu_system
     g.display = PicoGraphics(display=DISPLAY_PICO_DISPLAY_2 ,pen_type=PEN_P4) #, rotate= 90 test
     g.display.set_font("bitmap8")
-    g.display.set_backlight(0.7)
+    g.display.set_backlight(g.brightness)
     g.led.set_rgb(0, 0, 0)
-    g.screen_width, g.screen_height = g.display.get_bounds()
+    screen_width, screen_height = g.display.get_bounds()
     menu_system = {"defaults": {"bg_color": "black",
                                 "fg_color": "white",
                                 "alt_fg_color": "black",
                                 "alt_bg_color": "orange",
                                 "title_fg_color": "black",
                                 "title_bg_color": "white",
+                                "top_text_buffer": 5,
+                                "side_text_buffer": 5,
                                 "blink_rate": 0.5,
                                 "font": "bitmap8",
                                 "font_scale": 3,
                                 "font_height": 8,
                                 "option_lines": 5,
                                 "start_menu": "Splash"},
-                    "current_menu": "Splash",
+                    "current_menu": "Players",
                     "current_option": "Settings",
                     "Splash": {"type": "menu",
                                 "text": "Welcome to the game there is more text here to test the scroll function", 
@@ -86,8 +84,8 @@ def run_menu():
                                 "value": "Players"},
                     "Players": {"type": "option",
                                 "text": "Number of Players?",
-                                "options": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                                "value": 4},
+                                "options": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+                                "value": "2"},
                     "Brightness": {"type": "option",
                                     "text": "Screen Brightness?",
                                     "options": [0.2, 0.4, 0.6, 0.8, 1.0],
@@ -109,19 +107,8 @@ def run_menu():
             "orange" :{"rgb": ORANGE, "pen":g.display.create_pen(*ORANGE)},
             "grey" :{"rgb": GREY, "pen":g.display.create_pen(*GREY)},
             "cyan" :{"rgb": CYAN, "pen":g.display.create_pen(*CYAN)},
-                        "cyan" :{"rgb": BROWN, "pen":g.display.create_pen(*BROWN)}}
-    player_counts = [1,2,3,4,5,6,7,8,9,10]
-    players = [{"name":"P0", "start_time": 0, "bg_color": colors["grey"], "fg_color": colors["white"], "turns": [], "position" : 0},
-               {"name":"P1", "start_time": 0, "bg_color": colors["red"], "fg_color": colors["black"], "turns": [], "position" : 1},
-               {"name":"P2", "start_time": 0, "bg_color": colors["yellow"], "fg_color": colors["black"], "turns": [], "position" : 2},
-               {"name":"P3", "start_time": 0, "bg_color": colors["green"], "fg_color": colors["black"], "turns": [], "position" : 3},
-               {"name":"P4", "start_time": 0, "bg_color": colors["blue"], "fg_color": colors["black"], "turns": [], "position" : 4},
-               {"name":"P5", "start_time": 0, "bg_color": colors["black"], "fg_color": colors["white"], "turns": [], "position" : 5},
-               {"name":"P6", "start_time": 0, "bg_color": colors["white"], "fg_color": colors["black"], "turns": [], "position" : 6},
-               {"name":"P7", "start_time": 0, "bg_color": colors["purple"], "fg_color": colors["black"], "turns":[], "position" :7},
-               {"name":"P8", "start_time": 0, "bg_color": colors["pink"], "fg_color": colors["black"], "turns": [], "position" : 8},
-               {"name":"P9", "start_time": 0, "bg_color": colors["lime"], "fg_color": colors["black"], "turns": [], "position" : 9}]
-    first_time_setup = True
+            "brown" :{"rgb": BROWN, "pen":g.display.create_pen(*BROWN)}}
+
     shift = 0
     scroll_counter = 0
     scroll_title = ""
@@ -132,23 +119,23 @@ def run_menu():
         g.display.set_pen(colors[defaults["bg_color"]]["pen"])
         g.display.clear()
         
-        title_height =  int(g.screen_height / (1 + defaults["option_lines"]) * 1)
-        option_height = g.screen_height - title_height
+        title_height =  int(screen_height / (1 + defaults["option_lines"]) * 1)
+        option_height = screen_height - title_height
         # draw rectangle for title and title
         g.display.set_pen(colors[defaults["title_bg_color"]]["pen"])
-        g.display.rectangle(0, 0, g.screen_width, int(title_height))
+        g.display.rectangle(0, 0, screen_width, int(title_height))
         g.display.set_pen(colors[defaults["title_fg_color"]]["pen"])
         # scroll title if too long
         title_width = g.display.measure_text(current_menu["text"], defaults["font_scale"])
         scroll_title_width = g.display.measure_text(scroll_title, defaults["font_scale"])
         print("title width: ", title_width)
         print("scroll title width: ", scroll_title_width)
-        if scroll_title_width < g.screen_width:
+        if scroll_title_width < screen_width:
             scroll_title = " " + current_menu["text"]
         else:
             if scroll_counter % 2 == 0:
                 scroll_title = scroll_title[1:]
-        g.display.text(scroll_title, g.side_text_buffer, g.top_text_buffer, 100_000, scale = defaults["font_scale"]) 
+        g.display.text(scroll_title, defaults["side_text_buffer"], defaults["top_text_buffer"], 100_000, scale = defaults["font_scale"]) 
         
         line = 0
         display_options = current_menu["options"][shift:shift+defaults["option_lines"]]
@@ -161,17 +148,18 @@ def run_menu():
                     option = "^ " + option
                 if (time.time() % 2 == 0):
                     g.display.set_pen(colors[defaults["alt_bg_color"]]["pen"])
-                    g.display.rectangle(0, int(title_height + (line * row_height)), g.screen_width, int(row_height))
+                    g.display.rectangle(0, int(title_height + (line * row_height)), screen_width, int(row_height))
                     g.display.set_pen(colors[defaults["alt_fg_color"]]["pen"])
                 else:
                     option = option + " <<"
-            g.display.text(option, g.side_text_buffer, g.top_text_buffer + ((line+1) * row_height), g.screen_width, scale = defaults["font_scale"])
+            g.display.text(option, defaults["side_text_buffer"], defaults["top_text_buffer"] + ((line+1) * row_height), screen_width, scale = defaults["font_scale"])
             line += 1
             
         if button_x.read():
             if current_menu["options"][0] != current_menu["value"]:
                 current_menu["value"] = current_menu["options"][current_menu["options"].index(current_menu["value"]) - 1]
-                shift -= 1
+                if shift > 0:
+                    shift -= 1
 
         if button_y.read():
             # check for end of list then do nothing if value = last item
@@ -182,7 +170,7 @@ def run_menu():
         g.display.update()
         scroll_counter += 1
         time.sleep(0.1)
-        # print("Free RAM: ",free(True))
+        print("Free RAM: ",free(True))
 
 if __name__ == "__main__":
     run_menu()
